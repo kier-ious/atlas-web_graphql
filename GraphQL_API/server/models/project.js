5bc8e9e538d7ae319af2f406f014594d0f609ec4
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-07-03 14:27:54 -0500

    completed task 8, deleting arrays

--- GraphQL_API/server/models/project.js
+++ GraphQL_API/server/models/project.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const projectSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+const projectSchema = new Schema({
+  title: { type: String, required: true },
   description: { type: String },
   startDate: { type: Date, default: Date.now },
 });
--- GraphQL_API/server/models/task.js
+++ GraphQL_API/server/models/task.js
@@ -4,6 +4,7 @@ const taskSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   status: { type: String, enum: ['TODO', 'IN_PROGRESS', 'DONE'], default: 'TODO' },
+  projectId: { type: mongoose.Schema.Types.ObjectId, ref: 'Project' }
 });
 
 const Task = mongoose.model('Task', taskSchema);
